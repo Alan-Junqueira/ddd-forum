bfee1e7f97fdeb09e8f4b6509e0fd377c3be0fb3
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-07-27 23:07:25 -0300

    feat: Create case to fetch recent questions.

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,3 +1,4 @@
+import { IPaginationParams } from "@/core/repositories/pagination-params";
 import { Question } from "../../enterprise/entities/question";
 
 export interface IQuestionsRepository {
@@ -5,5 +6,6 @@ export interface IQuestionsRepository {
   delete(question: Question): Promise<void>
   findBySlug(slug: string): Promise<Question | null>
   findById(id: string): Promise<Question | null>
+  findManyRecent(params: IPaginationParams): Promise<Array<Question>>
   save(question: Question): Promise<void>
 }
