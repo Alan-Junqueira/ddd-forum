bf06cf136051a264097a96e7a0801f5e5681aa7b
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-08-31 22:36:45 -0300

    feat: Implement delete attachments functionality.

--- src/domain/forum/application/use-cases/delete-question.spec.ts
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -1,46 +1,74 @@
-import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository"
-import { makeQuestion } from "test/factories/make-question"
-import { DeleteQuestionUseCase } from "./delete-question"
-import { UniqueEntityId } from "@/core/entity/unique-entity-id"
-import { NotAllowedError } from "./errors/not-allowed-error"
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+import { makeQuestion } from "test/factories/make-question";
+import { DeleteQuestionUseCase } from "./delete-question";
+import { UniqueEntityId } from "@/core/entity/unique-entity-id";
+import { NotAllowedError } from "./errors/not-allowed-error";
+import { InMemoryQuestionAttachmentsRepository } from "test/repositories/in-memory-question-attachments-repository";
+import { makeQuestionAttachment } from "test/factories/make-question-attachment";
 
-let inMemoryQuestionsRepository: InMemoryQuestionsRepository
-let sut: DeleteQuestionUseCase
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository;
+let sut: DeleteQuestionUseCase;
 
-describe('Delete question', () => {
+describe("Delete question", () => {
   beforeEach(() => {
-    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
-    sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository)
-  })
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository();
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository
+    );
+    sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository);
+  });
 
-  it('Should be able to delete a question', async () => {
-    const newQuestion = makeQuestion({
-      authorId: new UniqueEntityId('author-1')
-    }, new UniqueEntityId('question-1'))
+  it("Should be able to delete a question", async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityId("author-1"),
+      },
+      new UniqueEntityId("question-1")
+    );
 
-    await inMemoryQuestionsRepository.create(newQuestion)
+    await inMemoryQuestionsRepository.create(newQuestion);
+
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityId("1"),
+      })
+    );
+
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityId("2"),
+      })
+    );
 
     await sut.execute({
-      questionId: 'question-1',
-      authorId: 'author-1'
-    })
+      questionId: "question-1",
+      authorId: "author-1",
+    });
 
-    expect(inMemoryQuestionsRepository.items).toHaveLength(0)
-  })
+    expect(inMemoryQuestionsRepository.items).toHaveLength(0);
+    expect(inMemoryQuestionAttachmentsRepository.items).toHaveLength(0);
+  });
 
-  it('Should no be able to delete a question from another user', async () => {
-    const newQuestion = makeQuestion({
-      authorId: new UniqueEntityId('author-1')
-    }, new UniqueEntityId('question-1'))
+  it("Should no be able to delete a question from another user", async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityId("author-1"),
+      },
+      new UniqueEntityId("question-1")
+    );
 
-    await inMemoryQuestionsRepository.create(newQuestion)
+    await inMemoryQuestionsRepository.create(newQuestion);
 
     const result = await sut.execute({
-      questionId: 'question-1',
-      authorId: 'author-2'
-    })
+      questionId: "question-1",
+      authorId: "author-2",
+    });
 
     expect(result.isLeft()).toBe(true);
     expect(result.value).toBeInstanceOf(NotAllowedError);
-  })
-})
+  });
+});
